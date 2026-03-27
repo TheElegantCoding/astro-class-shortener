e4e8bda0e88bce8ab00954deb3e571ecd364cb39
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-27 14:01:47 -0400

    refactor: streamline processBuild function and integrate options parameter in classShortener

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,12 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { processBuild } from '@src/util/process';
 
-import { createClassMap } from './util/class_map';
-import { findAllFiles } from './util/file';
-import { replaceInJS, replaceInCSS, replaceInHTML } from './util/replace_classes';
-import { scanForSelectors } from './util/scan_classes';
-
+import type { OptionType } from '@src/type/option_type';
 import type { AstroIntegration } from 'astro';
 
-const getFilesByExtension = (distributionPath: string) => {
-  const cssFiles = findAllFiles('.css', distributionPath);
-  const htmlFiles = findAllFiles('.html', distributionPath);
-  const jsFiles = findAllFiles('.js', distributionPath);
-  return { cssFiles, htmlFiles, jsFiles };
-};
-
-const processSelectors = (cssFiles: string[], htmlFiles: string[], jsFiles: string[]) => {
-  const allSelectors = new Set<string>();
-  scanForSelectors([
-    ...cssFiles,
-    ...htmlFiles,
-    ...jsFiles
-  ], allSelectors);
-  return allSelectors;
-};
-
-const replaceClassesAndWriteMap = (
-  cssFiles: string[],
-  htmlFiles: string[],
-  jsFiles: string[],
-  classMap: Record<string, string>,
-  jsonPath: string
-) => {
-  cssFiles.forEach((file) => { replaceInCSS(file, classMap); });
-  htmlFiles.forEach((file) => { replaceInHTML(file, classMap); });
-  jsFiles.forEach((file) => { replaceInJS(file, classMap); });
-  fs.writeFileSync(jsonPath, JSON.stringify(classMap, null, 2));
-};
-
-const processBuild = (directory: URL) => {
-  const distributionPath = fileURLToPath(directory);
-  const jsonPath = path.join(distributionPath, 'class_map.json');
-  const { cssFiles, htmlFiles, jsFiles } = getFilesByExtension(distributionPath);
-  const allSelectors = processSelectors(cssFiles, htmlFiles, jsFiles);
-  const classMap = createClassMap([...allSelectors]);
-
-  replaceClassesAndWriteMap(
-    cssFiles,
-    htmlFiles,
-    jsFiles,
-    classMap,
-    jsonPath
-  );
-};
-
-const classShortener = (): AstroIntegration => ({
+const classShortener = (options: OptionType): AstroIntegration => ({
   name: 'class-shortener',
   hooks: {
-    'astro:build:done': ({ dir: directory }) => {
-      processBuild(directory);
-    }
+    'astro:build:done': ({ dir: directory }) => { processBuild(directory, options); }
   }
 });
 
