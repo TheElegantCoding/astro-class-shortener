ea9257c9cd66c25b7b6db416249efdc6b7f8e27e
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-21 11:52:32 -0400

    chore: update eslint configuration to disable unsafe call rule and adjust rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable ts/no-unsafe-call */
 import eslintConfig from 'eslint-config-universal-code';
 
 const config = eslintConfig({
@@ -5,9 +6,13 @@ const config = eslintConfig({
   typescript: true,
   unicorn: true,
   perfectionist: true,
-  astro: true,
   json: true,
-  yml: true,
+  yml: true
+}, {
+  rules: {
+    'no-param-reassign': 'off',
+    'ts/no-dynamic-delete': 'off'
+  }
 });
 
 export default config;
